720524da3014a7d081b3986c2f91f8817d356c63
Wisam851 <wisam@bidecsol.com>
2025-09-01 08:17:10 -0700

    seeder fixes

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -13,7 +13,7 @@ class CategoryController extends Controller
 {
     public function index(Request $request)
     {
-        dd("awd");
+        // dd("awd");
         try {
             $query = Category::with(['parent','listings'])->latest();
 
