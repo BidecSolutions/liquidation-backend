dc17c97631a9524dbe30b3a56e20224a6403d4f0
Wisam851 <wisam@bidecsol.com>
2025-09-22 06:52:55 -0700

    Update UserAuthController.php

--- app/Http/Controllers/Api/UserAuthController.php
+++ app/Http/Controllers/Api/UserAuthController.php
@@ -691,28 +691,30 @@ class UserAuthController extends Controller
                 ], 400);
             }
         }else{
-            $code = rand(100000, 999999);
-            $user->verification_code = $code;
-            $user->verification_expires_at = now()->addMinutes(30);
-            $expiration = now()->addMinutes(30);
-            $user->save();
+           if($user->is_verified != 1){
+                $code = rand(100000, 999999);
+                $user->verification_code = $code;
+                $user->verification_expires_at = now()->addMinutes(30);
+                $expiration = now()->addMinutes(30);
+                $user->save();
 
-            $user->forceFill([
-                'verification_code'        => $code,
-                'verification_expires_at'  => $expiration,
-            ])->save();
+                $user->forceFill([
+                    'verification_code'        => $code,
+                    'verification_expires_at'  => $expiration,
+                ])->save();
 
-            // Send OTP email
-            Mail::send('emails.verification', ['user' => $user, 'code' => $code], function($message) use ($user){
-                $message->to($user->email)
-                        ->subject('Your Verification Code');
-            });
-            return response()->json([
-                'success' => false,
-                'message' => 'Emails is not verified yet', 
-                'email' => $user->email,
-                'is_verified' => 0,
-            ], 400);
+                // Send OTP email
+                Mail::send('emails.verification', ['user' => $user, 'code' => $code], function($message) use ($user){
+                    $message->to($user->email)
+                            ->subject('Your Verification Code');
+                });
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Emails is not verified yet', 
+                    'email' => $user->email,
+                    'is_verified' => 0,
+                ], 400);
+            }
         }
         if (!$user || !Hash::check($request->password, $user->password)) {
             return response()->json([
