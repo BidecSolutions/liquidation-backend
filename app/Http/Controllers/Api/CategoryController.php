7b4d08ab601f5759423477db27e13ba68b030ac2
Wisam851 <wisam@bidecsol.com>
2025-09-25 06:16:53 -0700

    Update CategoryController.php

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -20,8 +20,8 @@ class CategoryController extends Controller
 
 
             $query = Category::with(['parent:id,name,slug,parent_id','listings' => function($q) {
-                $q->withCount('views', 'watchers', 'children')->with('paymentMethod:id,name', 'shippingMethod:id,name');
-            }]);
+                $q->withCount('views', 'watchers')->with('paymentMethod:id,name', 'shippingMethod:id,name');
+            }])->withCount('children as child_count');
 
             if ($request->filled('parent_id')) {
                 $query->where('parent_id', $request->parent_id);
