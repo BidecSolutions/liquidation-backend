cced60d6d63ab944fe8b4f668f9e66ba01a1b281
Wisam851 <wisam@bidecsol.com>
2025-10-03 11:05:26 -0700

    added condition ENUMS in ran migrations changes in Store( ) and Update( )
    by Abdullah

---
+++ database/migrations/2025_10_03_172747_alter_condition_column_in_listings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('listings', function (Blueprint $table) {
+            $table->enum('condition', [
+                'new',
+                'used',
+                'brand_new',
+                'ready_to_move',
+                'under_construction',
+                'furnished',
+                'semi_furnished',
+                'unfurnished',
+                'recently_renovated'
+            ])->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('listings', function (Blueprint $table) {
+            $table->enum('condition', ['new', 'used'])->change();
+        });
+    }
+};
