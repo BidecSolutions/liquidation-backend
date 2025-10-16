ad738c78c101780deee966b3e1fd687b6cd2845c
Wisam851 <wisam@bidecsol.com>
2025-10-16 03:53:55 -0700

    s

--- resources/views/emails/notifications/reset-code.blade.php
+++ resources/views/emails/notifications/reset-code.blade.php
@@ -1,8 +1,8 @@
 @extends('emails.notifications.layout')
 
 @section('content')
-<!-- {{ if (!isset($user)) $user = auth()->user(); }} -->
-    <!-- <div class="greeting">Hello {{ $user->name ? $user->name : "Sir" }},</div> -->
+
+    <div class="greeting">Hello {{ $user->name}},</div>
 
     <div class="message">
         <p>You requested to reset your password on the Ma3rood App.</p>
