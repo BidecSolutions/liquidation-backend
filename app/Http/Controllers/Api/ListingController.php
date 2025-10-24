1b4513acf97f27a8d42fca39a4d6f2ac259c95c2
Wisam851 <wisam@bidecsol.com>
2025-10-24 06:30:23 -0700

    fixedaa

--- app/Http/Controllers/Api/ListingController.php
+++ app/Http/Controllers/Api/ListingController.php
@@ -197,7 +197,7 @@ class ListingController extends Controller
                     : explode(',', $request->status);
                 $query->whereIn('status', $statuses);
             }
-            return $query->count();
+            return $query->get();
 
             // 🔒 Filter by creator if authenticated (user guard)
             $authUserId = auth('api')->check() ? auth('api')->id() : null;
